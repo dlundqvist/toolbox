d3b628b543515385aa3bb41a5c2e2cef08bff618
Daniel Lundqvist <daniel@malarhojden.nu>
2026-05-15 13:47:43 +0200

    toolbox: Update source used to build GST.
    Was slightly out of date.

--- gst.c
+++ gst.c
@@ -1,9 +1,14 @@
 #include <devices/scsidisk.h>
 
+#include <dos/dos.h>
 #include <exec/exec.h>
 
+#include <proto/alib.h>
 #include <proto/dos.h>
 #include <proto/exec.h>
 
-#include <stdio.h>
+#include <limits.h>
+
+#include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
